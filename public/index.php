ae4626b77dd9143ab96087adec09f59022ef1bb1
James LaChance <fatboyxpc@gmail.com>
2018-05-04 22:34:28 -0400

    Add People object to index

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
-$people = json_decode(file_get_contents('https://x-24.io/DevTestData.json'));
+require '../vendor/autoload.php';
+
+use App\People;
+
+$peopleJson = 'https://x-24.io/DevTestData.json';
+$people = (new People($peopleJson))->sortBy('FName', People::SORT_ORDER_ASCENDING);
 
 include "../views/layout.php";
